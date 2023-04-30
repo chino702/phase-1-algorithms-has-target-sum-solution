26399c8f1c5fa55775e2e0cd4becb9a8d58b17f6
chino702 <chinox3702@gmail.com>
2023-04-30 16:25:31 -0700

    algo has target sum solution

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seen = new Set();
+  
+  for (const num of array) {
+    const complement = target - num;
+    
+    if (seen.has(complement)) {
+      return true;
+    }
+    
+    seen.add(num);
+  }
+  
+  return false;
 }
 
+
 /* 
-  Write the Big O time complexity of your function here
+ The time complexity of this function is O(n) because it iterates through the array once to build the hash table and then iterates through the array again to check if there is a pair that sums up to the target. The time taken by hash table operations is O(1) on average, so the time complexity of building the hash table is also O(n). Therefore, the overall time complexity of the function is O(n).
 */
 
 /* 
-  Add your pseudocode here
-*/
+Sort the array in ascending order.
+Initialize two pointers, left and right, to the first and last index of the array, respectively.
+While left is less than right:
+Compute the sum of the values at left and right indices.
+If the sum is equal to the target, return true.
+If the sum is greater than the target, decrement right.
+If the sum is less than the target, increment left.
+If the loop completes and no pair of values has a sum equal to the target, return false.*/
 
 /*
-  Add written explanation of your solution here
-*/
+This function is called hasTargetSum and it takes two arguments: an array of numbers and a target number. The goal of the function is to check if there are any two numbers in the array that add up to the target number. If there are, it should return true. If not, it should return false.
+
+To solve this problem, we can use a nested loop to check all possible pairs of numbers in the array. For each pair of numbers, we can add them together and check if the result is equal to the target number. If it is, we can return true. If we finish checking all possible pairs and none of them add up to the target number, we can return false.
+
+The time complexity of this function is O(n^2) because it has to check all possible pairs of numbers in the array.*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
